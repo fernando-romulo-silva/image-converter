71482bf4348cf4f68e8913fbfddba427fc3ab90e
Fernando Romulo da Silva <Fernando.Romulo@usr.brflt.corp>
2019-02-25 18:54:20 -0300

    news

--- src/test/java/org/imageconverter/ImageConvertControllerTest.java
+++ src/test/java/org/imageconverter/ImageConvertControllerTest.java
@@ -1,10 +1,11 @@
 package org.imageconverter;
 
+import static org.apache.commons.lang.StringUtils.deleteWhitespace;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 
-import org.apache.commons.lang.StringUtils;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +18,11 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClientException;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -33,6 +34,7 @@ public class ImageConvertControllerTest {
     @Autowired
     private TestRestTemplate restTemplate = new TestRestTemplate(new RestTemplateBuilder());
 
+    @Tag("integration")
     @Test
     public void convertTest() throws RestClientException {
 
@@ -40,16 +42,16 @@ public class ImageConvertControllerTest {
 
         final File imageFileData = new File(classLoader.getResource("image.png").getFile());
 
-        final MultiValueMap<String, Object> parameters = new LinkedMultiValueMap<String, Object>();
+        final var parameters = new LinkedMultiValueMap<String, Object>();
         parameters.add("file", new FileSystemResource(imageFileData));
 
         final HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "multipart/form-data");
         headers.set("Accept", "*/*");
 
-        final HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<MultiValueMap<String, Object>>(parameters, headers);
+        final var httpEntity = new HttpEntity<MultiValueMap<String, Object>>(parameters, headers);
 
-        final ResponseEntity<String> result = restTemplate.postForEntity("http://localhost:" + port + "/convert", httpEntity, String.class);
+        final var result = restTemplate.<String> postForEntity("http://localhost:" + port + "/convert", httpEntity, String.class);
 
         assertEquals(HttpStatus.OK.value(), result.getStatusCodeValue());
     }
@@ -61,7 +63,7 @@ public class ImageConvertControllerTest {
 
         final File imageFileData = new File(classLoader.getResource("image.png").getFile());
 
-        final MultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+        final var parameters = new LinkedMultiValueMap<String, Object>();
         parameters.add("file", new FileSystemResource(imageFileData)); // load file into parameter
         parameters.add("x", 885);
         parameters.add("y", 1417);
@@ -72,13 +74,13 @@ public class ImageConvertControllerTest {
         headers.set("Content-Type", "multipart/form-data");
         headers.set("Accept", "*/*");
 
-        final HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<MultiValueMap<String, Object>>(parameters, headers);
+        final var httpEntity = new HttpEntity<MultiValueMap<String, Object>>(parameters, headers);
 
-        final ResponseEntity<String> result = restTemplate.postForEntity("http://localhost:" + port + "/convertWithArea", httpEntity, String.class);
+        final var result = restTemplate.<String> postForEntity("http://localhost:" + port + "/convertWithArea", httpEntity, String.class);
 
         assertEquals(HttpStatus.OK.value(), result.getStatusCodeValue());
 
-        assertEquals("03399905748110000007433957701015176230000017040", StringUtils.deleteWhitespace(result.getBody()).replaceAll("[^x0-9]", ""));
+        assertEquals("03399905748110000007433957701015176230000017040", deleteWhitespace(result.getBody()).replaceAll("[^x0-9]", ""));
 
         result.getBody();
     }
