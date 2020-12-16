9e5d2c8f201ce4166e44fc025ce68ef4a890ca37
Fernando <fernando.romulo.silva@gmail.com>
2020-12-16 18:04:07 -0300

    news

--- image-converter-springboot/src/main/java/org/imageconverter/ImageConvertController.java
+++ image-converter-springboot/src/main/java/org/imageconverter/ImageConvertController.java
@@ -1,10 +1,11 @@
 package org.imageconverter;
 
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +17,18 @@ final class ImageConvertController {
     @Autowired
     private ImageConvertService imageConvertService;
 
-    @RequestMapping(value = "/convert", method = RequestMethod.POST, consumes = { "multipart/form-data" })
+    @RequestMapping(value = "/convert", method = POST, consumes = { "multipart/form-data" })
     public String convert(final @RequestParam MultipartFile file) {
         return imageConvertService.convert(new Image(file));
     }
 
-    @RequestMapping(value = "/convertWithArea", method = RequestMethod.POST, consumes = { "multipart/form-data" })
+    @RequestMapping(value = "/convertWithArea", method = POST, consumes = { "multipart/form-data" })
     public String convertWithArea(final @RequestParam MultipartFile file, @RequestParam final int x, @RequestParam final int y, @RequestParam final int width, @RequestParam final int height) {
         return imageConvertService.convert(new Image(file, x, y, width, height));
     }
     
     @GetMapping("/convertTestLive")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     public String ocr() {
         return "Convert live!";
     }
