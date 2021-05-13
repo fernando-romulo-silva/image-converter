3021cdcda041a17151f44e1686fbccd4578f69bf
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-05-13 09:07:14 -0300

    fghfghfg

--- image-converter-springboot/image-converter-springboot-all/src/main/java/org/imageconverter/Application.java
+++ image-converter-springboot/image-converter-springboot-all/src/main/java/org/imageconverter/Application.java
@@ -2,14 +2,12 @@ package org.imageconverter;
 
 import static org.springframework.boot.SpringApplication.run;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
-        run(Application.class, args);
+	run(Application.class, args);
     }
 }
